c0dc73b05566972388d48e5a006c7b7973c4814a
Zachery Hostens <zacheryph@gmail.com>
2015-10-30 07:57:53 -0500

    move helpers to test_helper

--- test/block_test.rb
+++ test/block_test.rb
@@ -1,13 +1,8 @@
 require 'minitest/autorun'
 
-def read_output
-  $stdout.rewind
-  $stdout.read
-end
-
 describe Defer do
   before do
-    $stdout = StringIO.new
+    reset_output
   end
 
   it "calls deferred blocks" do
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,3 +5,13 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'godefer'
 
 require 'minitest/autorun'
+
+# we use IO to verify things happening properly
+def reset_output
+  $stdout = StringIO.new
+end
+
+def read_output
+  $stdout.rewind
+  $stdout.read
+end
